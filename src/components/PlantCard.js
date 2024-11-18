17ed9a38ed354e9b109f3539d0aa7b06688a1a40
Joses Thuo <thuokjoses@gmail.com>
2024-11-18 20:42:32 +0300

    Final commit

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
-function PlantCard() {
+function PlantCard({ name, image, price }) {
+  const [isSoldOut, setIsSoldOut] = useState(false);
+
+  function handleSoldOutClick() {
+    setIsSoldOut(!isSoldOut);
+  }
+
   return (
+    // <li className={isSoldOut ? "sold-out" : ""}>
     <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
-      ) : (
-        <button>Out of Stock</button>
-      )}
+      <img src={image} alt={name} />
+      <h4>{name}</h4>
+      <p>Price: ${price}</p>
+      <button
+        className={isSoldOut ? "primary sold-out" : "primary"}
+        onClick={handleSoldOutClick}
+      >
+        {isSoldOut ? "Sold Out" : "In Stock"}
+      </button>
     </li>
   );
 }
